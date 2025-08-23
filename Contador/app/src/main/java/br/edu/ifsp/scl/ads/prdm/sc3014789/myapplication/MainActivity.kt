70570e9bcd13998299eaac82e7c079abbff6efd5
isaacnwt <isaacnewton02@hotmail.com>
2025-08-22 22:03:51 -0300

    refactor: usando o ActivityMainBinding

--- Contador/app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3014789/myapplication/MainActivity.kt
+++ Contador/app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3014789/myapplication/MainActivity.kt
@@ -1,32 +1,24 @@
 package br.edu.ifsp.scl.ads.prdm.sc3014789.myapplication
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
+import br.edu.ifsp.scl.ads.prdm.sc3014789.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var cliqueBt: Button
-    private lateinit var contadorTv: TextView
-    private val inicialEt: EditText by lazy {
-        findViewById(R.id.inicial_et)
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
     }
     private var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(amb.root)
 
-        contadorTv = findViewById(R.id.contador_tv)
-        cliqueBt = findViewById(R.id.clique_bt)
-
-
-        cliqueBt.setOnClickListener {
-            contadorTv.text = (++counter).toString()
+        amb.cliqueBt.setOnClickListener {
+            amb.contadorTv.text = (++counter).toString()
         }
 
-        inicialEt.addTextChangedListener{ p0 -> counter = p0.toString().toInt() }
+        amb.inicialEt.addTextChangedListener{ p0 -> counter = p0.toString().toInt() }
     }
 }
