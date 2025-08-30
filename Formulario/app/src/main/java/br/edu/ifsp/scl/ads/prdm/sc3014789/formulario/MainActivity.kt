74e95d2b411b70599596cfc944e3db2cc59f06ea
isaacnwt <isaacnewton02@hotmail.com>
2025-08-29 21:15:31 -0300

    feat(formulario): mostrando container dinamicamente

--- Formulario/app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3014789/formulario/MainActivity.kt
+++ Formulario/app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3014789/formulario/MainActivity.kt
@@ -1,6 +1,8 @@
 package br.edu.ifsp.scl.ads.prdm.sc3014789.formulario
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,5 +17,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainBinding.root)
+
+        activityMainBinding.estadoCivilSp.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val estadoCivil = resources.getStringArray(R.array.estado_civil)[position]
+                if (estadoCivil.equals("Casado(a)")) {
+                    activityMainBinding.conjugeLl.visibility = View.VISIBLE
+                } else {
+                    activityMainBinding.conjugeLl.visibility = View.GONE
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+        }
     }
 }
