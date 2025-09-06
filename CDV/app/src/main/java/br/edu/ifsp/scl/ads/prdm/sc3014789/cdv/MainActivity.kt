253c80c886274e72952346fbc78833e107476856
isaacnwt <isaacnewton02@hotmail.com>
2025-09-05 21:50:05 -0300

    feat(CDV): log mostrando o ciclo de vida

--- CDV/app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3014789/cdv/MainActivity.kt
+++ CDV/app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3014789/cdv/MainActivity.kt
@@ -1,20 +1,51 @@
 package br.edu.ifsp.scl.ads.prdm.sc3014789.cdv
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.prdm.sc3014789.cdv.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(amb.root)
+        Log.v(getString(R.string.app_name), "onCreate: Iniciando cdv COMPLETO")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v(getString(R.string.app_name), "onStart: Iniciando cdv VISÍVEL")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v(getString(R.string.app_name), "onResume: Iniciando cdv FOREGROUND")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v(getString(R.string.app_name), "onPause: Finalizando cdv FOREGROUND")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(getString(R.string.app_name), "onStop: Finalizando cdv VISÍVEL")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(getString(R.string.app_name), "onDestroy: Finalizando cdv COMPLETO")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.v(getString(R.string.app_name), "onRestart: preparando a execução do onRestart")
     }
 }
