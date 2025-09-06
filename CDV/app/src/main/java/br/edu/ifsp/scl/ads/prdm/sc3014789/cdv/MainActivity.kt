99a2abfd627802083a454caf923a84f095137792
isaacnwt <isaacnewton02@hotmail.com>
2025-09-05 22:21:31 -0300

    feat(CDV): persistindo dados na activity

--- CDV/app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3014789/cdv/MainActivity.kt
+++ CDV/app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3014789/cdv/MainActivity.kt
@@ -1,12 +1,16 @@
 package br.edu.ifsp.scl.ads.prdm.sc3014789.cdv
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.children
 import br.edu.ifsp.scl.ads.prdm.sc3014789.cdv.databinding.ActivityMainBinding
+import br.edu.ifsp.scl.ads.prdm.sc3014789.cdv.databinding.TileNameBinding
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
@@ -16,7 +20,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        amb.addNameBt.setOnClickListener {
+            TileNameBinding.inflate(layoutInflater).let { tileNameBinding ->
+                amb.moreNamesLl.addView(tileNameBinding.root)
+            }
+        }
+
         Log.v(getString(R.string.app_name), "onCreate: Iniciando cdv COMPLETO")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val nameList = mutableListOf<String>()
+        amb.moreNamesLl.children.forEach {
+            (it as EditText).text.toString().let { name ->
+                nameList.add(name)
+            }
+        }
+        outState.putStringArray("NAMES", nameList.toTypedArray())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedInstanceState.getStringArray("NAMES")?.forEach { name ->
+            TileNameBinding.inflate(layoutInflater).let { tileNameBinding ->
+                tileNameBinding.root.setText(name)
+                amb.moreNamesLl.addView(tileNameBinding.root)
+            }
+        }
     }
 
     override fun onStart() {
